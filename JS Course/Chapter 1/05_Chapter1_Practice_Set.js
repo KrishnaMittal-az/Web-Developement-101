1df53dd32187356c7fce1a1ab48ea4e37b87aa89
krishnamittal-az <krishnamittal30062004@gmail.com>
2024-10-29 11:37:58 +0530

    new files added

---
+++ JS Course/Chapter 1/05_Chapter1_Practice_Set.js
@@ -0,0 +1,20 @@
+// Chapter 1 - Q1
+let a = "Harry"
+let b = 6;
+console.log(a + b)
+
+//Q2
+console.log(typeof (b))
+console.log(typeof (a + b))
+
+//Q3
+//Q4
+const a1 = {
+    name: "Harry",
+    section: 1,
+    principal: false
+}
+a1['friend'] = "Mittal"
+console.log(a1)
+// a = "Harry"
+
