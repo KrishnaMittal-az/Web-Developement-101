61a4c0fb6e4920a43f5561e2a0deb69a0c34df08
krishnamittal-az <krishnamittal30062004@gmail.com>
2024-11-21 12:36:39 +0530

    new files added

--- Class150/JavaScript/Lecture_5/app.js
+++ Class150/JavaScript/Lecture_5/app.js
@@ -1,8 +1,29 @@
+// var a = 100;
+// console.log(a);
+//
+// function sam() {
+//     console.log("I am Krishna")
+// }
+//
+// sam();
+
+
+//--------------------------------------------------
+
+
 var a = 100;
-console.log(a);
 
 function sam() {
-    console.log("I am Krishna")
+    var b = 20;
+    console.log(b)
+
+    function lol() {
+        var c = 30;
+        console.log(c);
+    }
+
+    lol();
 }
 
+console.log(a);
 sam();
