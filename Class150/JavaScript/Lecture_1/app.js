d8f5efca606a7af39fb53d70ace6ea6fa8620c7d
krishnamittal-az <krishnamittal30062004@gmail.com>
2024-11-21 11:52:09 +0530

    new files added

---
+++ Class150/JavaScript/Lecture_1/app.js
@@ -0,0 +1,57 @@
+// let a = 10;
+// console.log(a);
+// console.log(10.5);
+//
+// //----------------------------------------------
+//
+// let b = 20;
+// console.log(a, b);
+//
+// //------------------------------------------------
+//
+// console.log(typeof a);
+// console.log(typeof b);
+//
+
+//---------------------------------------------------
+
+//string -> 3
+// ''   single Quotes
+// ""   Double Quotes
+// ``   Back Ticks
+
+// let a = 'Sam';
+// let b = "Krishna";
+// let c = `Mittal`;
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+//---------------------------------------------
+//Concatenation
+
+// let name = "Krishna";
+// let kaam = "time pass";
+// let state = "UP";
+// let out = "Hi I am " + name + " my job is to do " + kaam + " and I live in " + state;
+// console.log(out);
+//
+// // String Template literals
+//
+// let out2 = `Hi I am ${name} my job is to do ${kaam} and I live in ${state}`;
+// console.log(out2);
+
+//--------------------------------------------------------
+//
+// let a = 5;
+// let b = 2;
+// console.log(a / b);
+// console.log(typeof a / b);
+
+//---------------------------------------------------------
+
---
+++ Class150/JavaScript/Lecture_5/app.js
@@ -0,0 +1,8 @@
+var a = 100;
+console.log(a);
+
+function sam() {
+    console.log("I am Krishna")
+}
+
+sam();
